d1b55cad381a1e8d7ea0b0f8550e58fb18a1882c
Jerome Carniaux <brines_masher6n@icloud.com>
2026-04-12 21:07:39 -0400

    Fix: Fix the 5 LOW key items: Log retention, S3 versioning, Lambda layers, unused crawlers, error counting

--- terraform/modules/frontend/main.tf
+++ terraform/modules/frontend/main.tf
@@ -41,6 +41,8 @@ resource "aws_s3_bucket_lifecycle_configuration" "frontend" {
     id     = "expire-old-versions"
     status = "Enabled"
 
+    filter {}
+
     noncurrent_version_expiration {
       noncurrent_days = 30
     }
