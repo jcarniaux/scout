e85fc0467e010fe758f8d18cffbaa7d7d6318205
Jerome Carniaux <brines_masher6n@icloud.com>
2026-04-12 20:26:42 -0400

    Fix: Fixing the 10 MEDIUM issues

--- frontend/src/components/ErrorBoundary.tsx
+++ frontend/src/components/ErrorBoundary.tsx
@@ -23,7 +23,6 @@ export class ErrorBoundary extends Component<Props, State> {
   }
 
   componentDidCatch(error: Error, info: ErrorInfo) {
-    // eslint-disable-next-line no-console
     console.error('ErrorBoundary caught:', error, info.componentStack);
   }
 
